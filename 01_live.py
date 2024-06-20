5154f48a86f719501e4b3c2e020711b43923f6cd
Subrat Gupta <85225011+iSubrat@users.noreply.github.com>
2024-06-20 14:22:19 +0530

    Update 01_live.py

--- 01_live.py
+++ 01_live.py
@@ -75,7 +75,7 @@ def execute_query(db_host, db_username, db_password, db_database, query):
             while cursor.nextset():
                 pass
             values = read_file('values.json')
-            client = OpenAI(api_key=api_key)
+            client = OpenAI(api_key=openai_api_key)
             file_path = "demo-corporate.html"
             html_content = read_file(file_path)
             prompt = f"Description:\n{description}\n\n\nValues:\n{values}"
